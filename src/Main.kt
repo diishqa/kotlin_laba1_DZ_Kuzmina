953faf9a5ea1d24831a630d8ce6b7b325222e8df
diishqa <diana.34@list.ru>
2026-01-18 15:41:12 +0300

    Add Task data class withenumPriority

--- src/Main.kt
+++ src/Main.kt
@@ -10,3 +10,16 @@ fun main() {
     println("salary - ${employee.salary}")
     println("ezperience - ${employee.yearsOfExperience}")
 }
+enum class Priority {
+    low,
+    medium,
+    high
+}
+data class Task(
+    val title: String,
+    val description: String,
+    val priority: Priority,
+    var isCompleted: Boolean=false
+){
+
+}
