639dd8717a842fc2ab734feee3b7ec75507e0369
diishqa <diana.34@list.ru>
2026-01-19 15:15:02 +0300

    Demonstrate equals, copyandtoString for Task

--- src/Employee.kt
+++ src/Employee.kt
@@ -24,3 +24,17 @@ class Employee(
     fun getPosition()=position
 
 }
+
+enum class Priority {
+    low,
+    medium,
+    high
+}
+data class Task(
+    val title: String,
+    val description: String,
+    val priority: Priority,
+    var isCompleted: Boolean=false
+){
+
+}
--- src/Main.kt
+++ src/Main.kt
@@ -5,21 +5,27 @@ fun main() {
     employee.salary = 10
     employee.yearsOfExperience = 30
 
-    println("fio - ${employee.getFullName()}")
-    println("position - ${employee.getPosition()}")
-    println("salary - ${employee.salary}")
-    println("ezperience - ${employee.yearsOfExperience}")
-}
-enum class Priority {
-    low,
-    medium,
-    high
-}
-data class Task(
-    val title: String,
-    val description: String,
-    val priority: Priority,
-    var isCompleted: Boolean=false
-){
+//    println("fio - ${employee.getFullName()}")
+//    println("position - ${employee.getPosition()}")
+//    println("salary - ${employee.salary}")
+//    println("ezperience - ${employee.yearsOfExperience}")
+
+    val task1=Task("fix", "исправить", Priority.high)
+    val task2=Task("fix", "исправить", Priority.high)
+    val task3=Task("fix", "исправить", Priority.medium)
+    println("сравнение equals: ${task1.equals(task2)}")
+    println("сравнение equals: ${task1.equals(task3)}")
+
+    println("hashCode task1: ${task1.hashCode()}")
+    println("hashCode task2: ${task2.hashCode()}")
+    println("hashCode task3: ${task3.hashCode()}")
+
+    println(task1.toString())
+
+    val tCopy=task1.copy()
+    println(tCopy)
+
 
 }
+
+
